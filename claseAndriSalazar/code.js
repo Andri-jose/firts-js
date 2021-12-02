e3f5259fe99587c03b1da9e11633609fb940c29a
Andri-jose <85264938+Andri-jose@users.noreply.github.com>
2021-12-02 00:30:57 -0300

    Add files via upload

---
+++ claseAndriSalazar/code.js
@@ -0,0 +1,29 @@
+
+//se pide el nombre
+let nombre= prompt("Ingrese su nombre");
+
+alert( "Como te va? " + nombre);
+
+//se pide un numero y se parsea
+
+let numero= prompt("Ingrese su edad");
+
+n= parseInt(numero);
+
+let numero2= 50;
+
+alert(n + numero2);
+
+//se concatenan indicaciones que da el usuario
+
+let ciudad= prompt("Ingrese su ciudad de origen");
+
+let pais= prompt("Ingrese su pais de origen");
+
+coma= ","
+
+espacio= " ";
+
+origen= "Usted nacio en: " + ciudad + coma + espacio + pais;
+
+alert(origen)
